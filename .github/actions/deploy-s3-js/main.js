79416faef07b73db8e70395ef4b125e28a1cb442
anabbaa <ahmed.nabaa.an@gmail.com>
2023-04-11 01:41:38 +0200

    first js action

---
+++ .github/actions/deploy-s3-js/main.js
@@ -0,0 +1,8 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
+const run = ()=>{
+    core.notice("hello world");
+
+}
+run();
